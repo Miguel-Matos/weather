03d54f296b5815f5b99757ef929c92225d4cbedd
Miguel Matos <18700730+Miguel-Matos@users.noreply.github.com>
2023-05-02 13:16:52 +0900

    Added core content

--- src/api.js
+++ src/api.js
@@ -1,11 +1,11 @@
-import { content } from ".";
+import { content, form, getWeather } from ".";
 
 const api = (() => {
   const url = 'https://api.weatherapi.com/v1/current.json?key=02c5ebd19167484e92c102243233004&q='
-  let value = url + 'tokyo';
 
-  async function getInfo() {
+  async function getInfo(place) {
     try {
+      let value = url + place;
       const weather = await fetch(value, {mode: 'cors'});
       const response = await weather.json();
       return response;
@@ -15,14 +15,20 @@ const api = (() => {
     }
   } 
 
-  async function callAsync() {
-    const data = await getInfo();
+  async function callAsync(place) {
+    const data = await getInfo(place);
     document.getElementById('location').textContent = data.location.name;
+    document.getElementById('country').textContent = data.location.country;
+    document.getElementById('condition').textContent = data.current.condition.text;
+    document.getElementById('conditionImg').src = data.current.condition.icon;
+    document.getElementById('temp').textContent = `${data.current.temp_f}°F/${data.current.temp_c}°C`;
     console.log(data);
   }
-  callAsync();
+  callAsync('tokyo');
 
   return {getInfo, callAsync};
 })();
+
+
 
 export {api};
--- src/index.js
+++ src/index.js
@@ -3,18 +3,86 @@ import { api } from './api';
 
 // api.getInfo();
 
+const form = (() => {
+  const form = document.createElement('form');
+  const search = document.createElement('input');
+  search.type = 'text';
+  search.placeholder = 'Location';
+  search.setAttribute('id', 'search');
+  form.appendChild(search);
+
+  const submit = document.createElement('button');
+  submit.textContent = "Get Weather";
+  submit.setAttribute('id', 'submit');
+  form.appendChild(submit);
+
+  const cf = document.createElement('button');
+  cf.textContent = "C/F";
+  cf.setAttribute('id', 'cf');
+  form.appendChild(cf);
+
+  document.body.appendChild(form);
+
+  return {search, submit, cf};
+})();
+
 const content = (() => {
   const daWeather = document.createElement('h1');
   daWeather.textContent = "Da Weather";
   
   const location = document.createElement('h2');
   location.setAttribute('id', 'location');
+
+  const country = document.createElement('h4');
+  country.setAttribute('id', 'country');
+
+  const condition = document.createElement('p');
+  condition.setAttribute('id', 'condition');
   
+  const conditionImg = document.createElement('img');
+  conditionImg.setAttribute('id', 'conditionImg');
+
+  const temp = document.createElement('p');
+  temp.setAttribute('id', 'temp');
   
   document.body.appendChild(daWeather);
   document.body.appendChild(location);
+  document.body.appendChild(country);
+  document.body.appendChild(condition);
+  document.body.appendChild(conditionImg);
+  document.body.appendChild(temp);
 
-  return {daWeather, location};
+
+  return {daWeather, location, country, condition, conditionImg, temp};
 })();
 
-export {content}
+const getWeather = (() => {
+  // let f = true;
+  let place = undefined;
+  form.submit.addEventListener('click', (e) => {
+    e.preventDefault();
+
+    let value = form.search.value;
+    place = value;
+    api.callAsync(value);
+    form.search.value = '';
+  });
+
+  // form.cf.addEventListener('click', (e) => {
+  //   e.preventDefault();
+
+  //   if (f === true) {
+  //     f = false;
+  //     api.callAsync(place);
+  //     console.log('C');
+  //   } else {
+  //     f = true;
+  //     api.callAsync(place);
+  //     console.log('F');
+  //   }
+  // });
+
+  // return {f}
+})();
+
+export {content, form, getWeather}
